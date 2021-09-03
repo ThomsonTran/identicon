3759a38b076e40e79d6646e17b9a6b1b1cabf7ea
Thomson Tran <thomson.tran@gmail.com>
2021-09-03 06:53:21 -0400

    added image struct and  hash_input, pick_color, build_grid functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,27 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input()
+    |> pick_color()
+    |> build_grid()
   end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5,input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r,g,b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3,3,:discard)
+
+  end
+
 end
